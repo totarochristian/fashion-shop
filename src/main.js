745d4dabc7891d1fb9a5b88d04f024519ef422c5
totarochristian <totaro.christian.18@gmail.com>
2023-06-13 12:56:29 +0200

    Define basic route

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './assets/styles/general.scss'
 
+import {router} from './router';
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
